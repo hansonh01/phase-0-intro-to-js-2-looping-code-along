3a98999d005bf7db297a8dc727bd298c89b2d9c3
Hanson <hanson.hua903@gmail.com>
2023-05-20 02:23:54 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names,event="surprise"){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+function countDown(){
+    let countDown = 11;
+    while (countDown > 0) {
+        console.log((countDown--)-1);
+}};
